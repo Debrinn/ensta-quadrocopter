a32699f17ab4fa806859167725d71db2ad60e82c
mateo <contactpro.mateodreser@gmail.com>
2025-04-15 15:30:39 +0200

    Ajout de la fonction Malt_mult

--- helpers.c
+++ helpers.c
@@ -1,2 +1,19 @@
 # include "helpers.h"
 
+
+//Fonction qui multiplie les matrices
+
+void mat_mult(const float **A, int A_rows, int A_cols, const float **B, int B_cols, float **C){
+    //nombre de colonnes de A est égal au nombre de lignes de B
+    // On suppose que les matrices sont de bonne taille
+
+    for(int i = 0; i < A_rows ; i++){
+        for(int j = 0 ; j <  B_cols; j ++){
+            float res = 0.;
+            for (int k = 0; k < A_cols; k++){
+                res = res + A[i][k]* B[k][j]; 
+            }
+            C[i][j] = res ;
+        }
+    }
+}

86ab31c6a33c042079774c11261570d38d30e476
Noé BENGANA <noe.bengana@ensta-paris.fr>
2025-04-15 17:29:50 +0200

    Correction des const et des prototypes

--- helpers.c
+++ helpers.c
@@ -3,7 +3,7 @@
 #include "helpers.h"
 
 // Fonction qui multiplie les matrices
-void mat_mult(const float **A, int A_rows, int A_cols, const float **B, int B_cols, float **C) {
+void mat_mult( float **A, int A_rows, int A_cols,  float **B, int B_cols, float **C) {
     for (int i = 0; i < A_rows; i++) {
         for (int j = 0; j < B_cols; j++) {
             float res = 0.;
@@ -16,7 +16,7 @@ void mat_mult(const float **A, int A_rows, int A_cols, const float **B, int B_co
 }
 
 // Transposée d'une matrice
-void mat_transpose(const float **A, int rows, int cols, float **A_T) {
+void mat_transpose( float **A, int rows, int cols, float **A_T) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             A_T[j][i] = A[i][j];
@@ -25,14 +25,14 @@ void mat_transpose(const float **A, int rows, int cols, float **A_T) {
 }
 
 // Soustraction de vecteurs
-void vec_substract(const float *a, const float *b, float *c, int dim) {
+void vec_substract( float *a,  float *b, float *c, int dim) {
     for (int i = 0; i < dim; i++) {
         c[i] = a[i] - b[i];
     }
 }
 
 // Addition/soustraction de matrices
-void mat_algebrique(const float **A, const float **B, float **C, int rows, int cols, int op) {
+void mat_algebrique( float **A,  float **B, float **C, int rows, int cols, int op) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             C[i][j] = A[i][j] + op * B[i][j];
@@ -41,7 +41,7 @@ void mat_algebrique(const float **A, const float **B, float **C, int rows, int c
 }
 
 // Copie de matrice
-void copy_mat(const float **A, float **B, int rows, int cols) {
+void copy_mat( float **A, float **B, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             B[i][j] = A[i][j];
@@ -63,4 +63,15 @@ void free_mat(float **A, int rows) {
         free(A[i]); // Libère chaque ligne
     }
     free(A); // Puis libère le tableau de pointeurs
+}
+
+float norm(float **A,int rows,int cols){
+    float S=0;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){        
+            S+=(A[i][j])*(A[i][j]);
+        }
+    }
+    return S;
+
 }

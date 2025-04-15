7be1c35a0f88fc6c1dc3b7d83b942a449ea935c5
Noé BENGANA <noe.bengana@ensta-paris.fr>
2025-04-15 17:18:43 +0200

    Ajout d'opérations sur les matrices

--- helpers.c
+++ helpers.c
@@ -1,48 +1,66 @@
-# include "helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "helpers.h"
 
-
-//Fonction qui multiplie les matrices
-
-void mat_mult(const float **A, int A_rows, int A_cols, const float **B, int B_cols, float **C){
-    //nombre de colonnes de A est égal au nombre de lignes de B
-    // On suppose que les matrices sont de bonne taille
-
-    for(int i = 0; i < A_rows ; i++){
-        for(int j = 0 ; j <  B_cols; j ++){
+// Fonction qui multiplie les matrices
+void mat_mult(const float **A, int A_rows, int A_cols, const float **B, int B_cols, float **C) {
+    for (int i = 0; i < A_rows; i++) {
+        for (int j = 0; j < B_cols; j++) {
             float res = 0.;
-            for (int k = 0; k < A_cols; k++){
-                res = res + A[i][k]* B[k][j]; 
+            for (int k = 0; k < A_cols; k++) {
+                res += A[i][k] * B[k][j];
             }
-            C[i][j] = res ;
+            C[i][j] = res;
         }
     }
 }
 
-void mat_transpose(const float **A, int rows, int cols, float **A_T)
-{
-    for(int i=0;i<rows;i++){
-        for (int j=0;j<cols;j++){       // On inverse juste les coefficients , sans se soucier de la validité de la taille (on la suppose ok)
-            A_T[j][i]=A[i][j];
+// Transposée d'une matrice
+void mat_transpose(const float **A, int rows, int cols, float **A_T) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            A_T[j][i] = A[i][j];
         }
-    }
-
-}
-
-
-vec_substract(const float *a, const float *b, float *c, int dim){
-
-    for(int i=0; i<dim;i++){
-        c[i]=a[i]-b[i]; // Pas besoin de commentaire
     }
 }
 
-void copy_mat(const float **A,float **B,int rows, int cols){
+// Soustraction de vecteurs
+void vec_substract(const float *a, const float *b, float *c, int dim) {
+    for (int i = 0; i < dim; i++) {
+        c[i] = a[i] - b[i];
+    }
+}
 
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){        // Recopie une matrice non modifiable en matrice modifiable
-            B[i][j]=A[i][j];
+// Addition/soustraction de matrices
+void mat_algebrique(const float **A, const float **B, float **C, int rows, int cols, int op) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            C[i][j] = A[i][j] + op * B[i][j];
         }
     }
+}
 
+// Copie de matrice
+void copy_mat(const float **A, float **B, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            B[i][j] = A[i][j];
+        }
+    }
+}
 
+// Allocation dynamique de matrice 2D
+float **make_mat(int rows, int cols) {
+    float **A = malloc(rows * sizeof(float *));
+    for (int i = 0; i < rows; i++) {
+        A[i] = malloc(cols * sizeof(float));
+    }
+    return A;
+}
+
+void free_mat(float **A, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(A[i]); // Libère chaque ligne
+    }
+    free(A); // Puis libère le tableau de pointeurs
 }

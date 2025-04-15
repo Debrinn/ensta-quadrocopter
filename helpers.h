b6395e3258711f186bd2cc85e2881a828d093aeb
Noé BENGANA <noe.bengana@ensta-paris.fr>
2025-04-15 17:31:26 +0200

    Correction des const et des prototypes

--- helpers.h
+++ helpers.h
@@ -3,7 +3,12 @@
 #include <stdint.h>
 
 
-void mat_mult(const float *A, int A_rows, int A_cols, const float *B, int B_cols, float *C);
-void mat_transpose(const float *A, int rows, int cols, float *A_T);
-bool invert_3x3(const float A[3][3], float a_inv[3][3]); // Check that det(A) > 0 before inverting
-void vec_substract(const float *a, const float *b, float *c, int dim);
+void mat_mult( float **A, int A_rows, int A_cols,  float **B, int B_cols, float **C);
+void mat_transpose( float **A, int rows, int cols, float **A_T);
+bool invert_3x3( float A[3][3], float a_inv[3][3]); // Check that det(A) > 0 before inverting
+void vec_substract( float *a,  float *b, float *c, int dim);
+void mat_algebrique( float **A,  float **B, float **C, int rows, int cols, int op); // Sert à soustraire ou additionner deux matrices : op = 1 pour ajout et op = -1 pour soustraire
+void copy_mat( float **A, float **B, int rows, int cols);
+float **make_mat(int rows, int cols);
+void free_mat(float **A, int rows);
+float norm(float **A,int rows,int cols);
